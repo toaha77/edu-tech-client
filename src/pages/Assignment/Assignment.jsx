424e19e052b6e6f3d2d5fdda465d7a0be699eb56
toaha77 <istiaktoaha07@gmail.com>
2023-11-08 15:41:56 +0600

    pagination added

--- src/pages/Assignment/Assignment.jsx
+++ src/pages/Assignment/Assignment.jsx
@@ -1,26 +1,31 @@
 import { useEffect, useState } from "react";
 import AssignMentCard from "./AssignMentCard";
-import Swal from "sweetalert2";
-import { useLoaderData } from "react-router-dom";
+ import { useLoaderData } from "react-router-dom";
 
 const Assignment = () => {
   const [assignments, setAssignments] = useState([]);
   const [currentPage, setCurrentPage] = useState(0)
   const [itemsPerPage, setItemsPerPage] = useState(4);
   const { count } = useLoaderData();
-    const  numberofPages = Math.ceil(count / itemsPerPage)
-  const pages = [...Array(numberofPages).keys()]
-  const url = `http://localhost:5000/create-assignment?page=${currentPage}&&size=&{itemsPerPage}`;
+    const  numberOfPages = Math.ceil(count / itemsPerPage)
+
+
+  const pages = [...Array(numberOfPages).keys()]
+
+  
+ ;
   useEffect(() => {
-    fetch(url)
+    fetch(`http://localhost:5000/create-assignment?page=${currentPage}&size=${itemsPerPage}`)
       .then((res) => res.json())
       .then((data) => setAssignments(data));
-  }, []);
+  }, [currentPage, itemsPerPage]);
 
    
 
   const handleLevel = e =>{
-    
+   const val = parseInt(e.target.value)
+    setItemsPerPage(val)
+    setCurrentPage(0)
   } 
   return (
     <div className="mt-4">
@@ -32,7 +37,7 @@ const Assignment = () => {
       <div className="mb-4">
         <label htmlFor="pet-select">Choose a level:</label>
 
-        <select value={itemsPerPage} className="input input-bordered" onChange={handleLevel} name="pets">
+        <select className="input input-bordered"  name="pets">
           <option value="">--Please choose an option--</option>
           <option value="Hard">Hard</option>
           <option value="Medium">Medium</option>
@@ -50,13 +55,18 @@ const Assignment = () => {
       </div>
     <div className="flex items-center justify-center mt-6">
       {
-        pages.map(page => <button  
+        pages.map(page => <button
 
           onClick={()=> setCurrentPage(page)}
           key={page} className={
-            currentPage === page  && 'btn bg-red-500 text-white hover:bg-red-500 text-white'
+            currentPage === page  ? 'btn mr-2 bg-red-500 text-white hover:bg-red-500 text-white' : 'btn'
           }>{page}</button>)
       }
+      <select value={itemsPerPage} onChange={handleLevel} className="input input-bordered" name="" id="">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+      </select>
     </div>
     </div>
   );
